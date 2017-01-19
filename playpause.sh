87bb51ad4e548b6277ba3868a149595443bb5153
Chris West (Faux) <git@goeswhere.com>
2017-01-19 21:53:06 +0000

    playpause prefers halting mplayer, which it doesn't seem to mind too much

--- playpause.sh
+++ playpause.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-if pgrep deadbeef; then
+if PID=$(pgrep --oldest mplayer); then
+    if [ "T" = "$(cut -d' ' -f 3 /proc/${PID}/stat)" ]; then
+        SIG=CONT
+    else
+        SIG=STOP
+    fi
+    kill -${SIG} $PID
+elif pgrep deadbeef; then
     deadbeef --toggle-pause
 else
     dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
