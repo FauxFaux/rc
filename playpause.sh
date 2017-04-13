4a9befbbc84ae4c387f8769335978f9422be68ba
Chris West (Faux) <git@goeswhere.com>
2017-04-13 01:38:56 +0100

    revert signal control of mplayer: worse than useless

--- playpause.sh
+++ playpause.sh
@@ -1,13 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-if PID=$(pgrep --oldest mplayer); then
-    if [ "T" = "$(cut -d' ' -f 3 /proc/${PID}/stat)" ]; then
-        SIG=CONT
-    else
-        SIG=STOP
-    fi
-    kill -${SIG} $PID
-elif pgrep deadbeef; then
+if pgrep deadbeef; then
     deadbeef --toggle-pause
 else
     dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
