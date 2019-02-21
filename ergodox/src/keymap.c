a38000d80212ab03c033a9e60af4a29d15a12853
Chris West (Faux) <git@goeswhere.com>
2019-02-21 19:56:48 +0000

    ergodox: faux cole9 KXAX
    Attempting to get pause/break on the keyboard,
    and meta keys consistent between qwerty and colemak.
    Also, just give up and bind all the blank meta keys
    to duplicates, just in case.
    
    Still hitting cases where I need to press something dumb.

--- ergodox/src/keymap.c
+++ ergodox/src/keymap.c
@@ -4,27 +4,133 @@
 #include "version.h"
 #include "keymap_german.h"
 #include "keymap_nordic.h"
+#include "keymap_french.h"
+#include "keymap_spanish.h"
+
+#define LCGS(code) LCTL(LGUI(LSFT(code)))
+#define LCS(code) LCTL(LSFT(code))
 
 enum custom_keycodes {
-  PLACEHOLDER = SAFE_RANGE, // can always be here
-  EPRM,
-  RGB_SLD,
+  PLACEHOLDER = SAFE_RANGE,              // can always be here
+  EPRM,                   
+  RGB_SLD,                
   
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_ergodox(KC_GRAVE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_TRANSPARENT,KC_LGUI,KC_Q,KC_W,KC_F,KC_P,KC_G,KC_UP,KC_BSPACE,KC_A,KC_R,KC_S,KC_T,KC_D,KC_LSPO,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_LEFT,KC_LCTRL,MO(1),KC_TRANSPARENT,KC_LGUI,MO(2),KC_DELETE,KC_HOME,KC_END,KC_SPACE,KC_LALT,KC_ESCAPE,KC_TRANSPARENT,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_DOWN,KC_J,KC_L,KC_U,KC_Y,KC_SCOLON,KC_EQUAL,KC_H,KC_N,KC_E,KC_I,KC_O,KC_QUOTE,KC_RIGHT,KC_K,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,MO(1),KC_LGUI,ALGR_T(KC_APPLICATION),KC_RCTRL,TG(2),KC_INSERT,KC_MEDIA_PLAY_PAUSE,KC_PGUP,KC_PGDOWN,KC_TAB,KC_ENTER),
+  [0] = LAYOUT_ergodox(
+    // left hand
+    KC_GRV,                 KC_1,                   KC_2,                   KC_3,                   KC_4,                   KC_5,                   KC_PSCR,                
+    KC_LCMD,                KC_Q,                   KC_W,                   KC_F,                   KC_P,                   KC_G,                   KC_UP,                  
+    KC_BSPC,                KC_A,                   KC_R,                   KC_S,                   KC_T,                   KC_D,                   
+    KC_LSPO,                KC_Z,                   KC_X,                   KC_C,                   KC_V,                   KC_B,                   KC_LEFT,                
+    KC_LCTL,                MO(1),                  KC_LALT,                KC_LCMD,                MO(2),                  
+                                                                                                                            KC_DEL,                 KC_HOME,                
+                                                                                                                                                    KC_END,                 
+                                                                                                    KC_SPC,                 KC_LALT,                KC_ESC,                 
+        // right hand
+        KC_PAUS,                KC_6,                   KC_7,                   KC_8,                   KC_9,                   KC_0,                   KC_MINS,                
+        KC_DOWN,                KC_J,                   KC_L,                   KC_U,                   KC_Y,                   KC_SCLN,                KC_EQL,                 
+                                KC_H,                   KC_N,                   KC_E,                   KC_I,                   KC_O,                   KC_QUOT,                
+        KC_RGHT,                KC_K,                   KC_M,                   KC_COMM,                KC_DOT,                 KC_SLSH,                KC_RSPC,                
+        MO(1),                  KC_RCMD,                RALT_T(KC_APP),         _______,                KC_RCTL,                
+        KC_INS,                 KC_MPLY,                
+        KC_PGUP,                
+        KC_PGDN,                KC_TAB,                 KC_ENT
+    ),
 
-  [1] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_CIRC,KC_AMPR,KC_ASTR,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,LSFT(KC_NONUS_BSLASH),KC_LCBR,KC_RCBR,LSFT(KC_NONUS_HASH),KC_TRANSPARENT,KC_TRANSPARENT,KC_NONUS_BSLASH,KC_LBRACKET,KC_RBRACKET,KC_NONUS_HASH,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(3),TO(4),KC_TRANSPARENT,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_CALCULATOR,KC_TRANSPARENT,KC_TRANSPARENT,KC_WWW_SEARCH,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PERC,KC_CIRC,KC_AMPR,KC_ASTR,KC_TRANSPARENT,KC_TRANSPARENT,KC_EXLM,KC_PERC,KC_HASH,KC_DLR,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(0),KC_TRANSPARENT,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_AUDIO_VOL_UP,KC_AUDIO_VOL_DOWN,KC_TRANSPARENT,KC_TRANSPARENT),
 
-  [2] = LAYOUT_ergodox(KC_TRANSPARENT,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_BTN2,KC_MS_UP,KC_MS_BTN1,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MS_LEFT,KC_MS_DOWN,KC_MS_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RESET,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_TRANSPARENT,KC_7,KC_8,KC_9,KC_KP_MINUS,KC_TRANSPARENT,KC_F12,KC_4,KC_5,KC_6,KC_KP_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_1,KC_2,KC_3,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(0),TO(0),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+  [1] = LAYOUT_ergodox(
+    // left hand
+    _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+    _______,                _______,                _______,                KC_CIRC,                KC_AMPR,                KC_ASTR,                _______,                
+    _______,                _______,                LSFT(XXXXXXXNUS_BSLASH),KC_LCBR,                KC_RCBR,                LSFT(XXXXXXXNUS_HASH),  
+    _______,                _______,                XXXXXXXNUS_BSLASH,      KC_LBRC,                KC_RBRC,                XXXXXXXNUS_HASH,        _______,                
+    _______,                _______,                TO(3),                  TO(4),                  TO(2),                  
+                                                                                                                            KC_MPRV,                KC_MNXT,                
+                                                                                                                                                    KC_CALC,                
+                                                                                                    _______,                _______,                KC_WSCH,                
+        // right hand
+        _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+        _______,                _______,                KC_PERC,                KC_CIRC,                KC_AMPR,                KC_ASTR,                _______,                
+                                _______,                KC_EXLM,                KC_PERC,                KC_HASH,                KC_DLR,                 _______,                
+        _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+        _______,                _______,                _______,                TO(0),                  _______,                
+        KC_MUTE,                _______,                
+        KC_VOLU,                
+        KC_VOLD,                _______,                _______
+    ),
 
-  [3] = LAYOUT_ergodox(KC_GRAVE,KC_1,KC_2,KC_3,KC_4,KC_5,KC_TRANSPARENT,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_UP,KC_BSPACE,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_LEFT,KC_LCTRL,KC_NONUS_BSLASH,KC_TRANSPARENT,KC_LGUI,KC_TRANSPARENT,KC_DELETE,KC_HOME,KC_END,KC_SPACE,KC_LALT,KC_ESCAPE,KC_PSCREEN,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_DOWN,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_EQUAL,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,KC_RIGHT,KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSHIFT,KC_NONUS_HASH,KC_TRANSPARENT,ALGR_T(KC_APPLICATION),TO(0),KC_RCTRL,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,KC_PGUP,KC_PGDOWN,KC_TAB,KC_ENTER),
 
-  [4] = LAYOUT_ergodox(KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RGB_MOD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_VAD,RGB_VAI,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,TO(0),KC_TRANSPARENT,RGB_TOG,RGB_SLD,KC_TRANSPARENT,KC_TRANSPARENT,RGB_HUD,RGB_HUI),
+  [2] = LAYOUT_ergodox(
+    // left hand
+    _______,                KC_F1,                  KC_F2,                  KC_F3,                  KC_F4,                  KC_F5,                  _______,                
+    _______,                _______,                KC_BTN2,                KC_MS_U,                KC_BTN1,                _______,                _______,                
+    _______,                _______,                KC_MS_L,                KC_MS_D,                KC_MS_R,                _______,                
+    _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+    _______,                _______,                _______,                _______,                _______,                
+                                                                                                                            _______,                _______,                
+                                                                                                                                                    _______,                
+                                                                                                    _______,                _______,                _______,                
+        // right hand
+        _______,                KC_F6,                  KC_F7,                  KC_F8,                  KC_F9,                  KC_F10,                 KC_F11,                 
+        _______,                KC_7,                   KC_8,                   KC_9,                   KC_PMNS,                _______,                KC_F12,                 
+                                KC_4,                   KC_5,                   KC_6,                   KC_PPLS,                _______,                _______,                
+        _______,                KC_1,                   KC_2,                   KC_3,                   _______,                _______,                _______,                
+        KC_P0,                  KC_PDOT,                _______,                TO(0),                  _______,                
+        _______,                _______,                
+        _______,                
+        _______,                _______,                _______
+    ),
+
+
+  [3] = LAYOUT_ergodox(
+    // left hand
+    KC_GRV,                 KC_1,                   KC_2,                   KC_3,                   KC_4,                   KC_5,                   _______,                
+    KC_TAB,                 KC_Q,                   KC_W,                   KC_E,                   KC_R,                   KC_T,                   KC_UP,                  
+    KC_BSPC,                KC_A,                   KC_S,                   KC_D,                   KC_F,                   KC_G,                   
+    KC_LSFT,                KC_Z,                   KC_X,                   KC_C,                   KC_V,                   KC_B,                   KC_LEFT,                
+    KC_LCTL,                XXXXXXXNUS_BSLASH,      KC_LALT,                KC_LCMD,                _______,                
+                                                                                                                            KC_DEL,                 KC_HOME,                
+                                                                                                                                                    KC_END,                 
+                                                                                                    KC_SPC,                 KC_LALT,                KC_ESC,                 
+        // right hand
+        KC_PSCR,                KC_6,                   KC_7,                   KC_8,                   KC_9,                   KC_0,                   KC_MINS,                
+        KC_DOWN,                KC_Y,                   KC_U,                   KC_I,                   KC_O,                   KC_P,                   KC_EQL,                 
+                                KC_H,                   KC_J,                   KC_K,                   KC_L,                   KC_SCLN,                KC_QUOT,                
+        KC_RGHT,                KC_N,                   KC_M,                   KC_COMM,                KC_DOT,                 KC_SLSH,                KC_RSFT,                
+        XXXXXXXNUS_HASH,        KC_RCMD,                RALT_T(KC_APP),         TO(0),                  KC_RCTL,                
+        KC_INS,                 KC_MPLY,                
+        KC_PGUP,                
+        KC_PGDN,                _______,                KC_ENT
+    ),
+
+
+  [4] = LAYOUT_ergodox(
+    // left hand
+    _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+    _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+    _______,                _______,                _______,                _______,                _______,                _______,                
+    _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+    _______,                _______,                _______,                _______,                _______,                
+                                                                                                                            RGB_MOD,                _______,                
+                                                                                                                                                    _______,                
+                                                                                                    RGB_VAD,                RGB_VAI,                _______,                
+        // right hand
+        RESET,                  _______,                _______,                _______,                _______,                _______,                _______,                
+        _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+                                _______,                _______,                _______,                _______,                _______,                _______,                
+        _______,                _______,                _______,                _______,                _______,                _______,                _______,                
+        _______,                _______,                _______,                TO(0),                  _______,                
+        RGB_TOG,                RGB_SLD,                
+        _______,                
+        _______,                RGB_HUD,                RGB_HUI
+    ),
+
 
 };
 
+bool suspended = false;
 const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_TOGGLE(1)
 };
--- ergodox/src/rules.mk
+++ ergodox/src/rules.mk
@@ -1,2 +1,3 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+COMMAND_ENABLE = no
