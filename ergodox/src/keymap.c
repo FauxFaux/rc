5945608e2e367833b9376ae54c2e8141e234c0d0
Chris West (Faux) <git@goeswhere.com>
2019-02-21 20:13:46 +0000

    ergodox: faux cole9 YL0a
    Again, try and align qwerty and colemak on meta keys.
    Add scroll lock to qwerty, just in case.
    Use numpad keys for numpad keys again, and bind numlock,
    the aim is to have maximum flexibility, not convenience.

--- ergodox/src/keymap.c
+++ ergodox/src/keymap.c
@@ -73,10 +73,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                                                                                     _______,                
                                                                                                     _______,                _______,                _______,                
         // right hand
-        _______,                KC_F6,                  KC_F7,                  KC_F8,                  KC_F9,                  KC_F10,                 KC_F11,                 
-        _______,                KC_7,                   KC_8,                   KC_9,                   KC_PMNS,                _______,                KC_F12,                 
-                                KC_4,                   KC_5,                   KC_6,                   KC_PPLS,                _______,                _______,                
-        _______,                KC_1,                   KC_2,                   KC_3,                   _______,                _______,                _______,                
+        KC_NLCK,                KC_F6,                  KC_F7,                  KC_F8,                  KC_F9,                  KC_F10,                 KC_F11,                 
+        _______,                KC_P7,                  KC_P8,                  KC_P9,                  KC_PMNS,                _______,                KC_F12,                 
+                                KC_P4,                  KC_P5,                  KC_P6,                  KC_PPLS,                _______,                _______,                
+        _______,                KC_P1,                  KC_P2,                  KC_P3,                  _______,                _______,                _______,                
         KC_P0,                  KC_PDOT,                _______,                TO(0),                  _______,                
         _______,                _______,                
         _______,                
@@ -86,16 +86,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_ergodox(
     // left hand
-    KC_GRV,                 KC_1,                   KC_2,                   KC_3,                   KC_4,                   KC_5,                   _______,                
+    KC_GRV,                 KC_1,                   KC_2,                   KC_3,                   KC_4,                   KC_5,                   KC_PSCR,                
     KC_TAB,                 KC_Q,                   KC_W,                   KC_E,                   KC_R,                   KC_T,                   KC_UP,                  
     KC_BSPC,                KC_A,                   KC_S,                   KC_D,                   KC_F,                   KC_G,                   
     KC_LSFT,                KC_Z,                   KC_X,                   KC_C,                   KC_V,                   KC_B,                   KC_LEFT,                
-    KC_LCTL,                XXXXXXXNUS_BSLASH,      KC_LALT,                KC_LCMD,                _______,                
+    KC_LCTL,                XXXXXXXNUS_BSLASH,      KC_LALT,                KC_LCMD,                KC_SLCK,                
                                                                                                                             KC_DEL,                 KC_HOME,                
                                                                                                                                                     KC_END,                 
                                                                                                     KC_SPC,                 KC_LALT,                KC_ESC,                 
         // right hand
-        KC_PSCR,                KC_6,                   KC_7,                   KC_8,                   KC_9,                   KC_0,                   KC_MINS,                
+        KC_PAUS,                KC_6,                   KC_7,                   KC_8,                   KC_9,                   KC_0,                   KC_MINS,                
         KC_DOWN,                KC_Y,                   KC_U,                   KC_I,                   KC_O,                   KC_P,                   KC_EQL,                 
                                 KC_H,                   KC_J,                   KC_K,                   KC_L,                   KC_SCLN,                KC_QUOT,                
         KC_RGHT,                KC_N,                   KC_M,                   KC_COMM,                KC_DOT,                 KC_SLSH,                KC_RSFT,                
